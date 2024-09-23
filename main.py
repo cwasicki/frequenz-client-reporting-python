0882dafa49036d278d871547e87a668a5e2fc349
cwasicki <126617870+cwasicki@users.noreply.github.com>
2024-09-23 21:26:04 +0200

    Add sdk bridge

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from frequenz.client.reporting.component_graph import ComponentGraph, Component, Connection, ComponentCategory, InverterType
 import json
+from frequenz.client.reporting import ReportingApiClient
+from frequenz.client.reporting.sdk_reporting_bridge import list_microgrid_components_data_receiver
+import asyncio
+from datetime import datetime
 
-
+from frequenz.client.common.metric import Metric
 def build_graph(json_data: dict) -> ComponentGraph:
     components = []
     connections = []
@@ -29,7 +33,7 @@ def build_graph(json_data: dict) -> ComponentGraph:
     return ComponentGraph(components, connections)
 
 
-def main():
+async def main():
     # Read JSON data from file
     with open("comps13.json", "r") as file:
         data = json.load(file)
@@ -40,5 +44,27 @@ def main():
     # Print component graph
     print(component_graph)
 
+    key = open("key.txt", "r").read().strip()
+    client = ReportingApiClient(server_url="grpc://reporting.api.frequenz.com:443?ssl=true", key=key)
+
+    microgrid_id = 13
+    component_ids = [256, 258]
+    microgrid_components = [
+        (microgrid_id, component_ids),
+    ]
+    start_dt = datetime(2024, 9, 17)
+    end_dt = datetime(2024, 9, 18)
+    resolution = 900
+
+    async for sample in list_microgrid_components_data_receiver(
+        client,
+        microgrid_components=microgrid_components,
+        metrics=[Metric.AC_ACTIVE_POWER],
+        start_dt=start_dt,
+        end_dt=end_dt,
+        resolution=resolution,
+    ):
+        print(sample)
+
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
